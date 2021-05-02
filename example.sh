eecdae1885c7020dbd09a0d43185add8923faa09
Hannan Ali <ali.abdulhannan@gmail.com>
2021-05-03 01:30:33 +0500

    remove unnecessary variables in example.sh

--- example.sh
+++ example.sh
@@ -1,8 +1,4 @@
 ### Generates an Example Configuration. Requires a Linux AMD64 Binary
-ARCH=amd64
-OS=linux
-NAME=ipsec-conf-generator-${ARCH}-${OS}
-
 mkdir -p releases
 
 ./releases/ipsec-protonvpn-cli-linux-amd64 \
